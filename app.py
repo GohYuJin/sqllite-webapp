234068b1864a9b4002b50d3febfdc4c2175c5100
TriEightz <yujin_g1995@hotmail.com>
2024-03-10 18:45:45 +0800

    add dashboard

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import datetime
 import sqlite3
+from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 
 currentDateTime = datetime.datetime.now()
 app = Flask(__name__)
@@ -57,13 +59,58 @@ def clear():
     conn.close()
     return(render_template("clear.html"))
 
-@app.route("/dashboard",methods=["GET","POST"])
-def dashbaord():   
-    return(render_template("dashboard.html"))
+#@app.route("/dashboard",methods=["GET","POST"])
+#def dashboard():   
+#    return(render_template("dashboard.html"))
+
+def query_count_employees():
+    conn = sqlite3.connect('database1.db')
+    cursor = conn.cursor()
+    table_name = 'employee'
+    query = f"SELECT COUNT(*) FROM {table_name}"
+    cursor.execute(query)
+    employee_count = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return employee_count
+
+
+@callback(Output('live-update-text', 'children'),
+          Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+    count = query_count_employees()
+    return [
+        dbc.Card(
+            dbc.CardBody([
+                 html.H2("Name Count", className="card-title", style={'textAlign': 'center'}),
+                 html.H1('{0}'.format(count), className="card-text", style={'textAlign': 'center', "fontSize": "100px"}),
+            ]), style={"width": "30rem"}
+        ),        
+        html.Br(),
+        dbc.Button(
+            "Go Back", id="back", className="back", external_link=True, href="/main",
+        ),
+    ]
+
+
+dashboard = Dash(__name__, server=app, url_base_pathname="/dash/", external_stylesheets=[dbc.themes.CYBORG])
+dashboard.layout = html.Div([
+        html.Div(id='live-update-text'),
+        dcc.Interval(
+            id='interval-component',
+            interval=5*1000, # in milliseconds
+            n_intervals=0
+        )
+    ], style={"display":"flex", "justify-content": "center", "align-items": "center", "margin-top": "100px"})
 
 @app.route("/end",methods=["GET","POST"])
 def end():  
     return(render_template("end.html"))
 
+@app.route('/dashboard',methods=["GET","POST"]) 
+def dashboard():
+    return redirect('/dash')
+
 if __name__ == "__main__":
     app.run()
+    
